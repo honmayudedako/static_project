eee36e41e4caf4109502090d5fe62b3265356ccf
本間瑠 <yudedako0213@gmail.com>
2023-09-26 23:31:09 +0900

    second commit

---
+++ src/main/java/practice/PracticeFor.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class PracticeFor {
+
+	public static void main(String[] args) {
+		
+		int total = 0;
+		
+		for (int i = 1; i < 101; i++) {
+			total = total + i;
+		}
+		
+		System.out.println(total);
+	}
+
+}
---
+++ src/main/java/practice/PracticeForEach.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class PracticeForEach {
+
+	public static void main(String[] args) {
+		
+		String[] employee = { "大友", "富田", "小野田", "佐藤", "松井", "阿部", "本間", "田原", "福尾", "江藤" };
+		
+		for (String list : employee) {
+			System.out.println(list);
+		}
+	}
+	
+}
