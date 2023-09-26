3b16503ade180440250c09ab7cc8a5f52203136b
本間瑠 <yudedako0213@gmail.com>
2023-09-26 16:18:58 +0900

    first commit

---
+++ src/main/java/practice/Sample.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Sampleです");
+		System.out.println("Java");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
